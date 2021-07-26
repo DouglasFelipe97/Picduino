ba1d7bceffdd6a8f51e49b1d947bc0c7fddacc22
DouglasFelipe97 <86208209+DouglasFelipe97@users.noreply.github.com>
2021-07-25 21:09:11 -0300

    Add files via upload

---
+++ Termometro DHT22.X/build/default/production/LCD4B.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/LCD4B.d  \
+ build/default/production/LCD4B.p1:  \
+ LCD4B.c  \
+LCD4B.h 
---
+++ Termometro DHT22.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Termometro_DHT22.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Termometro_DHT22.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=termometrodht22.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/termometrodht22.x.tar
